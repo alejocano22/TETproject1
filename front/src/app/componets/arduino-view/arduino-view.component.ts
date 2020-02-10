41df2e56bcc15dc7e8cf330ffb5670e839933e94
Alejandro Cano <alejocano22@gmail.com>
2020-02-09 21:20:42 -0500

    logout

--- front/src/app/componets/arduino-view/arduino-view.component.ts
+++ front/src/app/componets/arduino-view/arduino-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { arduino } from 'src/app/models/arduino.model';
+import { Router } from "@angular/router";
+import * as myGlobals from '../globals';
 import { arduinoService as arduinoService } from 'src/app/services/arduino.service';
 
 @Component({
@@ -11,13 +13,20 @@ export class ArduinoViewComponent implements OnInit {
 
   public arduinoArray: arduino[] = [];
 
-  constructor(public arduinoServices: arduinoService) { }
+  constructor(public arduinoServices: arduinoService, public router: Router) { }
 
   ngOnInit() {
     this.arduinoArray = [];
     this.getArduino();
     console.log(this.arduinoArray);
   }
+
+  logout() {
+    localStorage.removeItem('token');
+    let path = '/login';
+    this.router.navigate([path]);
+  }
+
 
   public async getArduino() {
     this.arduinoServices.getArduino().subscribe((data: any) => {
